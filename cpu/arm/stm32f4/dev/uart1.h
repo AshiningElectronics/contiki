d9d3f904854dc973c5323c14372ae8e05360588d
Benjamin Vedder <benjamin@vedder.se>
2013-10-03 16:29:13 +0200

    Added the STM32F4 cpu

---
+++ cpu/arm/stm32f4/dev/uart1.h
@@ -0,0 +1,12 @@
+/*
+ * This file exists as a work-around for the hardware dependant calls
+ * to slip_arch_init.  
+ */
+
+#ifndef DEV_UART1_H
+#define DEV_UART1_H
+
+#define BAUD2UBR(x) x
+
+#endif
+
